43f1a555801cd589bb260b258a3d9b6e63fbad71
kewatvinod9936 <159791676+kewatvinod9936@users.noreply.github.com>
2024-02-15 08:25:40 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,44 @@
+     
+<footer class="text-center">
+    Copyright &copy; by <a href="#"> Narmada Path</a>. All Right Reserved From 2024 
+</footer>     
+</div>
+      
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/code.js"></script>
+ 	<script src="./js/jquery.js"></script>
+	<script src="./js/bootstrap.min.js"></script>
+	<script src="./js/summernote.js"></script>
+	
+	<script type="text/javascript">						    
+	<!--
+	
+	$(document).ready(function() {
+		
+		$('textarea').summernote({
+		    height: 200
+		});
+	
+	
+		$('#submitBtn').click(function() {
+			var summernoteContent = $('.summernote').summernote('code');
+			$('#textarea').val(summernoteContent);
+		});
+	
+	});
+	
+	//-->
+	</script>
+  </body>
+</html>
+
+
+<!-- <script>
+        CKEDITOR. replace( 'content', {
+            height: 300,
+            filebrowserUploadUrl: "media.php"
+        });
+    </script>-->
